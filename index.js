2e645374c09ec8331598c11df2629b1f8de9f4b6
Ramses David Baralt Useche <165963667+Bramses125@users.noreply.github.com>
2025-02-02 18:45:10 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,16 +5,27 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
 
+<<<<<<< HEAD
 // Middleware
+=======
+>>>>>>> 1f0024e (put y delete)
 app.use(cors());
 app.use(bodyParser.json());
 
 const pool = new Pool({
+<<<<<<< HEAD
   user: 'tu_usuario', 
   host: 'localhost',
   database: 'likeme',
   password: 'tu_contraseña', 
   port: 5432,
+=======
+    user: 'tu_usuario', 
+    host: 'localhost',
+    database: 'likeme',
+    password: 'tu_contraseña', 
+    port: 5432,
+>>>>>>> 1f0024e (put y delete)
 });
 
 app.get('/posts', async (req, res) => {
@@ -22,6 +33,7 @@ app.get('/posts', async (req, res) => {
     const result = await pool.query('SELECT * FROM posts');
     res.json(result.rows);
   } catch (error) {
+<<<<<<< HEAD
         res.json({ error: error.message });
   }
 });
@@ -44,4 +56,54 @@ app.post('/posts', async (req, res) => {
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
+=======
+    res.json({ error: error.message });
+  }
+});
+
+app.post('/posts', async (req, res) => {
+  const { titulo, img, descripcion } = req.body;
+
+  try {
+    const query = 'INSERT INTO posts (titulo, img, descripcion, likes) VALUES ($1, $2, $3, 0)';
+    const values = [titulo, img, descripcion];
+    const result = await pool.query(query, values);
+
+    res.json(result.rows);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+app.put('/posts/like/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const query = 'UPDATE posts SET likes = likes + 1 WHERE id = $1';
+    const values = [id];
+    const result = await pool.query(query, values);
+
+    res.json(result.rows);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+app.delete('/posts/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const query = 'DELETE FROM posts WHERE id = $1';
+    const values = [id];
+    const result = await pool.query(query, values);
+
+    res.json(result.rows);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+>>>>>>> 1f0024e (put y delete)
 });
